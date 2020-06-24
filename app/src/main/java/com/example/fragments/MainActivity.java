5e76e2670e0c9e8ecd6100b7b3b38b5b2bdf04aa
VictorSA11 <viko_1995@hotmail.com>
2020-06-23 21:02:01 -0600

    commit2

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.fragments;
-import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
 
@@ -17,28 +18,31 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinner = (Spinner)findViewById(R.id.spinner);
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
-               Fragment FragmentoSeleccionado = null;
-
-                switch (pos){
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                Fragment FragmentoSeleccionado = null;
+                switch (pos) {
                     case 0:
-                        FragmentoSeleccionado  = new ListaFragmento();
+                        FragmentoSeleccionado = new ListaFragmento();
                         break;
                     case 1:
-                        FragmentoSeleccionado  = new GridFragmento();
+                        FragmentoSeleccionado = new GridFragmento();
+                        break;
 
                 }
-                FragmentManager fragmentManager = getFragmentManager();
+                FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction transaction = fragmentManager.beginTransaction();
-               // transaction.replace(R.id.fragmento,FragmentoSeleccionado);
+                transaction.replace(R.id.fragmento, FragmentoSeleccionado);
                 transaction.commit();
-
-
             }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
         });
     }
+
 }
